0a734c62968004b56da3cd1a6163b991ec596f60
Bikossor <andre.lichtenthaeler@gmail.com>
2022-11-06 14:42:40 +0100

    chore: remove css files
    #20

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,6 @@
 import { ChakraProvider } from "@chakra-ui/react";
 import React from "react";
 import ReactDOM from "react-dom/client";
-import "./index.css";
 import { RouterProvider } from "react-router-dom";
 import { Router } from "./Router";
 
