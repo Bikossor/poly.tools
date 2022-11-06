49f03ea8da73456af29882135c42d96a537cb2a4
Bikossor <andre.lichtenthaeler@gmail.com>
2022-11-06 16:27:04 +0100

    build: open browser on startup in development mode
    #20

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,9 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
-export default defineConfig({
+export default defineConfig(({ mode }) => ({
   plugins: [react()],
-});
+  server: {
+    open: mode === "development",
+  },
+}));
