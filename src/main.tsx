9849b32e69bbf0f54cfb2c9b10866fd375b14cd7
Bikossor <andre.lichtenthaeler@gmail.com>
2022-11-06 12:09:29 +0100

    chore: remove `App.tsx`
    #20

--- src/main.tsx
+++ src/main.tsx
@@ -1,16 +1,14 @@
 import { ChakraProvider } from "@chakra-ui/react";
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./App";
 import "./index.css";
 import { RouterProvider } from "react-router-dom";
 import { Router } from "./Router";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
-    <RouterProvider router={Router} />
     <ChakraProvider>
-      <App />
+      <RouterProvider router={Router} />
     </ChakraProvider>
   </React.StrictMode>,
 );
