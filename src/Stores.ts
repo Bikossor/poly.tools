c22222d59de6d7fe28ba78f7cc18dd7a4211c4cf
Bikossor <andre.lichtenthaeler@gmail.com>
2022-11-06 12:40:19 +0100

    feat: create `useMenuDrawerStore`
    #20

---
+++ src/Stores.ts
@@ -0,0 +1,13 @@
+import create from "zustand";
+
+type MenuDrawerStore = {
+  readonly isOpen: boolean;
+  readonly close: () => void;
+  readonly open: () => void;
+};
+
+export const useMenuDrawerStore = create<MenuDrawerStore>(set => ({
+  isOpen: false,
+  close: () => set(() => ({ isOpen: false })),
+  open: () => set(() => ({ isOpen: true })),
+}));
