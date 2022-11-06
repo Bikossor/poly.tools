047b99942a9897f51b6a82d451db5b6363e89e69
Bikossor <andre.lichtenthaeler@gmail.com>
2022-11-06 14:45:44 +0100

    feat: style `Layout` component
    #20

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -2,15 +2,26 @@ import { Button } from "@chakra-ui/react";
 import { Outlet } from "react-router-dom";
 import { useMenuDrawerStore } from "../Stores";
 import { MenuDrawer } from "./MenuDrawer";
+import { HamburgerIcon } from "@chakra-ui/icons";
 
 export const Layout = () => {
   const openMenuDrawer = useMenuDrawerStore(state => state.open);
 
   return (
-    <>
-      <Button onClick={openMenuDrawer}>Open Menu</Button>
+    <div style={{ height: "100vh" }}>
+      <Button onClick={openMenuDrawer}>
+        <HamburgerIcon />
+      </Button>
       <MenuDrawer />
-      <Outlet />
-    </>
+      <main
+        style={{
+          paddingInline: "1rem",
+          maxWidth: "40rem",
+          marginInline: "auto",
+        }}
+      >
+        <Outlet />
+      </main>
+    </div>
   );
 };
