e45b65b945d8dcf186d77f05406f51d662a948e1
Bikossor <andre.lichtenthaeler@gmail.com>
2022-11-13 01:30:38 +0100

    feat: use and configure vite pwa plugin
    #32

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,15 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import { VitePWA } from "vite-plugin-pwa";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  plugins: [react()],
+  plugins: [
+    react(),
+    VitePWA({
+      registerType: "autoUpdate",
+    }),
+  ],
   server: {
     open: mode === "development",
   },
