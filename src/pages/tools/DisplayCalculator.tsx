434a2ba549e4bb9051e0718fde2c54c6bdea8e3b
André Lichtenthäler <andre.lichtenthaeler@gmail.com>
2022-11-18 01:07:29 +0100

    feat(display-calculator): add input labels and copy button (#37)
    * feat: add input labels
    
    #36
    
    * feat: add copy button
    
    #36

--- src/pages/tools/DisplayCalculator.tsx
+++ src/pages/tools/DisplayCalculator.tsx
@@ -1,5 +1,12 @@
-import { Heading, Input } from "@chakra-ui/react";
-import { useEffect, useState } from "react";
+import {
+  Button,
+  Heading,
+  Input,
+  InputGroup,
+  InputRightElement,
+  Text,
+} from "@chakra-ui/react";
+import { ReactNode, useEffect, useState } from "react";
 
 const calcPixelDensity = (
   resHorizontal: number,
@@ -10,6 +17,18 @@ const calcPixelDensity = (
     Math.sqrt(Math.pow(resHorizontal, 2) + Math.pow(resVertical, 2)) / diagonal
   );
 };
+
+type WithChildren = { children: ReactNode };
+
+const InputGroupColumn = ({ children }: WithChildren) => (
+  <InputGroup flexDirection={"column"}>{children}</InputGroup>
+);
+
+const TextSpan = ({ children }: WithChildren) => (
+  <Text as={"span"}>{children}</Text>
+);
+
+const handleCopyClick = (data: string) => navigator.clipboard.writeText(data);
 
 export const DisplayCalculator = () => {
   const [resHorizontal, setResHorizontal] = useState(1920);
@@ -29,30 +48,50 @@ export const DisplayCalculator = () => {
       <Heading as={"h1"}>Display Calculator</Heading>
 
       <div style={{ display: "grid", rowGap: "2rem" }}>
-        <Input
-          value={resHorizontal}
-          type="number"
-          inputMode="numeric"
-          onChange={event => setResHorizontal(parseInt(event.target.value, 10))}
-        />
-        <Input
-          value={resVertical}
-          type="number"
-          inputMode="numeric"
-          onChange={event => setResVertical(parseInt(event.target.value, 10))}
-        />
-        <Input
-          value={diagonal}
-          type="number"
-          inputMode="decimal"
-          onChange={event => setDiagonal(parseFloat(event.target.value))}
-        />
-        <Input
-          variant="filled"
-          type="number"
-          inputMode="decimal"
-          value={pixelDensity.toFixed(3)}
-        />
+        <InputGroupColumn>
+          <TextSpan children={"Horizontal resolution"} />
+          <Input
+            value={resHorizontal}
+            type="number"
+            inputMode="numeric"
+            onChange={event =>
+              setResHorizontal(parseInt(event.target.value, 10))
+            }
+          />
+        </InputGroupColumn>
+        <InputGroupColumn>
+          <TextSpan children={"Vertical resolution"} />
+          <Input
+            value={resVertical}
+            type="number"
+            inputMode="numeric"
+            onChange={event => setResVertical(parseInt(event.target.value, 10))}
+          />
+        </InputGroupColumn>
+        <InputGroupColumn>
+          <TextSpan children={"Diagonal"} />
+          <Input
+            value={diagonal}
+            type="number"
+            inputMode="decimal"
+            onChange={event => setDiagonal(parseFloat(event.target.value))}
+          />
+        </InputGroupColumn>
+        <InputGroupColumn>
+          <TextSpan children={"Pixel density"} />
+          <Input
+            variant="filled"
+            type="number"
+            inputMode="decimal"
+            value={pixelDensity.toFixed(3)}
+            readOnly
+          />
+          <InputRightElement>
+            <Button onClick={() => handleCopyClick(pixelDensity.toFixed(3))}>
+              Copy
+            </Button>
+          </InputRightElement>
+        </InputGroupColumn>
       </div>
     </>
   );
