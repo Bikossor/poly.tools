e397c9135a0b09ada6a5a87e62801eb4b029e239
Bikossor <andre.lichtenthaeler@gmail.com>
2023-02-19 16:48:51 +0100

    feat: improve layout and menu button placement
    #45

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,31 +1,30 @@
-import { Button } from "@chakra-ui/react";
+import { HamburgerIcon } from "@chakra-ui/icons";
+import { Button, Container, VStack } from "@chakra-ui/react";
 import { Outlet } from "react-router-dom";
 import { useMenuDrawerStore } from "../Stores";
 import { MenuDrawer } from "./MenuDrawer";
-import { HamburgerIcon } from "@chakra-ui/icons";
 
 export const Layout = () => {
   const openMenuDrawer = useMenuDrawerStore(state => state.open);
 
   return (
     <div style={{ height: "100vh" }}>
-      <Button
-        onClick={openMenuDrawer}
-        style={{ position: "fixed", left: "1rem", top: "1rem" }}
-      >
-        <HamburgerIcon />
-      </Button>
+      <VStack>
+        <Container
+          display={"flex"}
+          alignItems={"center"}
+          height={16}
+          position={"relative"}
+        >
+          <Button onClick={openMenuDrawer}>
+            <HamburgerIcon />
+          </Button>
+        </Container>
+        <Container>
+          <Outlet />
+        </Container>
+      </VStack>
       <MenuDrawer />
-      <main
-        style={{
-          paddingInline: "5rem",
-          maxWidth: "40rem",
-          marginInline: "auto",
-          marginBlock: "4rem",
-        }}
-      >
-        <Outlet />
-      </main>
     </div>
   );
 };
