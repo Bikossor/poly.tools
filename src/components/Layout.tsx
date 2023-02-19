9429a7dbaadc1d2e498d6b2f580aedfb02164b9e
André Lichtenthäler <andre.lichtenthaeler@gmail.com>
2023-02-19 16:52:38 +0100

    feat(layout): improve layout and menu button placement (#46)
    * feat: improve layout and menu button placement
    
    #45
    
    * feat: change button to iconbutton
    
    #45

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,31 +1,33 @@
-import { Button } from "@chakra-ui/react";
+import { HamburgerIcon } from "@chakra-ui/icons";
+import { Button, Container, IconButton, VStack } from "@chakra-ui/react";
 import { Outlet } from "react-router-dom";
 import { useMenuDrawerStore } from "../Stores";
 import { MenuDrawer } from "./MenuDrawer";
-import { HamburgerIcon } from "@chakra-ui/icons";
 
 export const Layout = () => {
   const openMenuDrawer = useMenuDrawerStore(state => state.open);
 
   return (
     <div style={{ height: "100vh" }}>
-      <Button
-        onClick={openMenuDrawer}
-        style={{ position: "fixed", left: "1rem", top: "1rem" }}
-      >
-        <HamburgerIcon />
-      </Button>
+      <VStack>
+        <Container
+          display={"flex"}
+          alignItems={"center"}
+          height={16}
+          position={"relative"}
+        >
+          <IconButton
+            icon={<HamburgerIcon />}
+            title="Open menu"
+            aria-label="Open menu"
+            onClick={openMenuDrawer}
+          />
+        </Container>
+        <Container>
+          <Outlet />
+        </Container>
+      </VStack>
       <MenuDrawer />
-      <main
-        style={{
-          paddingInline: "5rem",
-          maxWidth: "40rem",
-          marginInline: "auto",
-          marginBlock: "4rem",
-        }}
-      >
-        <Outlet />
-      </main>
     </div>
   );
 };
