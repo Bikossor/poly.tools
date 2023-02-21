24c4dae56daca30f53925b0500c064829cdaf5a4
Bikossor <andre.lichtenthaeler@gmail.com>
2023-02-21 23:14:53 +0100

    feat: add prompt to update PWA
    #55

--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,7 @@ import { ChakraProvider, ColorModeScript } from "@chakra-ui/react";
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { RouterProvider } from "react-router-dom";
+import { ReloadPrompt } from "./components/ReloadPrompt";
 import { Router } from "./Router";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
@@ -9,6 +10,7 @@ ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
     <ChakraProvider>
       <ColorModeScript type={"localStorage"} />
       <RouterProvider router={Router} />
+      <ReloadPrompt />
     </ChakraProvider>
   </React.StrictMode>,
 );
